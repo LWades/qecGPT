2eaf5758a07659a9d06169a22664e1402e9fb618
lwades <601149711@qq.com>
2024-03-28 14:43:37 +0800

    dl

--- qec/decoding/gen_trndt_pe.py
+++ qec/decoding/gen_trndt_pe.py
@@ -74,7 +74,7 @@ mod2 = mod2(device=device, dtype=dtype)
 # ps = torch.linspace(0.01, 0.10, 10)
 # ps = torch.linspace(0.16, 0.20, 5)
 # ps = torch.linspace(0.11, 0.15, 5)
-ps = torch.linspace(0.04, 0.06, 3)
+ps = torch.linspace(0.10, 0.20, 10)
 # ps = [0.09]
 log("error rate list: {}".format(ps))
 log("Code: {}, d: {}, seed: {}, error model: {}, trnsz: {}".format(c_type, d, code_seed, 'depolarized', trnsz))
@@ -127,7 +127,7 @@ batch_size = 10000
 # nohup python gen_trndt_pe.py -c_type 'sur' --d 3 --trnsz 10000 > logs/gen_trndt_pe_04.log &
 # python gen_trndt_pe.py -c_type 'sur' --d 5 --trnsz 10000000
 
-def gen_batch_eval():
+def gen_batch_torc_eval():
     import h5py
     batch_nums = trnsz // batch_size
     # log("batch_size: {}".format(batch_size))
@@ -177,7 +177,7 @@ def gen_batch_eval():
                 # log("Num {} batch end.".format(i))
         log("Error rate {} success!".format(p))
 
-def gen_batch():
+def gen_batch_torc():
     import h5py
     batch_nums = trnsz // batch_size
     # log("batch_size: {}".format(batch_size))
@@ -228,6 +228,7 @@ def gen_batch():
 
 
 def gen_img_batch():
+    log("gen_img_batch")
     import h5py
     batch_nums = trnsz // batch_size
     log("batch_size: {}".format(batch_size))
@@ -269,9 +270,9 @@ def gen_img_batch():
                 log("logical errors trans to 1d end.")
 
                 log("Origin syndrome trans to image syndrome start...")
-                image_syndromes = torch.empty(batch_size, 2 * d - 1, 2 * d - 1)
+                image_syndromes = np.empty((batch_size, 2 * d - 1, 2 * d - 1))
                 for j in range(len(syndromes)):
-                    image_syndromes[j] = trans_org_syndr_to_image_syndr(d, syndromes[j])
+                    image_syndromes[j] = ZX2image(d, syndromes[j])
                 log("Origin syndrome trans to image syndrome end.")
 
                 log("Writing image syndromes to h5py file start...")
@@ -289,6 +290,7 @@ def gen_img_batch():
 
 
 def gen_img_batch_eval():
+    log("gen_img_batch_eval")
     batch_nums = trnsz // batch_size
     log("batch_size: {}".format(batch_size))
     for p in ps:
@@ -348,7 +350,7 @@ def gen_img_batch_eval():
         log("Error rate {} success!".format(p))
 
 
-gen_batch()
+gen_img_batch()
 # gen_img_batch_eval()
 # gen_batch_eval()
 # nohup python gen_trndt_pe.py -c_type 'sur' --d 5 --trnsz 10000000 > logs/gen_trndt_pe_batch.log &
@@ -366,4 +368,5 @@ gen_batch()
 # nohup python gen_trndt_pe.py --c_type 'sur' --d 3 --k 1 --trnsz 10000 --seed 1 > logs/gen_trndt_pe_sur_d3_eval_s1.log &
 # nohup python gen_trndt_pe.py --c_type 'torc' --d 5 --k 2 --trnsz 10000 --seed 1 > logs/gen_trndt_pe_torc_d5_eval_s1.log &
 # nohup python gen_trndt_pe.py --c_type 'sur' --d 5 --k 1 --trnsz 10000 --seed 1 > logs/gen_trndt_pe_sur_d5_eval_s1.log &
+# nohup python gen_trndt_pe.py --c_type 'sur' --d 3 --k 1 --trnsz 10000000 --seed 0 > logs/gen_trndt_pe_sur_d3_s0.log &
 # nohup python gen_trndt_pe.py --c_type 'torc' --d 5 --k 2 --trnsz 10000000 > logs/gen_trndt_pe_torc_d5.log &
