13b7d066a9c6850e937b726e8c051176b0c205a4
lwades <601149711@qq.com>
2024-03-31 17:40:33 +0800

    change gtp.py

--- qec/decoding/gen_trndt_pe.py
+++ qec/decoding/gen_trndt_pe.py
@@ -245,51 +245,51 @@ def gen_img_batch():
             dataset_le = f.create_dataset('logical_errors', shape=(0, 1), maxshape=(None, 1), chunks=True, compression="gzip")
             log("Dataset create success")
             for i in tqdm(range(batch_nums)):
-                log("Num {} batch start..., {} in all".format(i, batch_nums))
+                # log("Num {} batch start..., {} in all".format(i, batch_nums))
 
                 E = Errormodel(e_rate=p, e_model='depolarized')
-                log("Errors generating start...")
+                # log("Errors generating start...")
                 errors = E.generate_error(n=code.n, m=batch_size, seed=seed)     # 这里改成了 batch_size
-                log("Errors generating end.")
+                # log("Errors generating end.")
 
-                log("Syndromes generating start...")
+                # log("Syndromes generating start...")
                 syndromes = mod2.commute(errors, code.g_stabilizer)
-                log("Syndromes generating end.")
+                # log("Syndromes generating end.")
 
-                log("Pure errors generating start...")
+                # log("Pure errors generating start...")
                 pes = E.pure(code.pure_es, syndromes, device=device, dtype=dtype)  # 得到可以恢复错误症状翻转的纯错误
-                log("Pure errors generating end.")
+                # log("Pure errors generating end.")
 
                 # recovers = pes
-                log("Check generating start...")
+                # log("Check generating start...")
                 check = mod2.opt_prod(pes, errors)  # 矩阵加法
-                log("Check generating end.")
-                log("Logical errors generating start...")
+                # log("Check generating end.")
+                # log("Logical errors generating start...")
                 logical_errors = mod2.commute(check, code.logical_opt)  # 看看有无逻辑错误
-                log("Logical errors generating end.")
+                # log("Logical errors generating end.")
 
-                log("logical errors trans to 1d start...")
+                # log("logical errors trans to 1d start...")
                 logical_errors_1d = logical_errors[:, 0] * 2 + logical_errors[:, 1]
                 logical_errors_1d = logical_errors_1d.unsqueeze(1)
-                log("logical errors trans to 1d end.")
+                # log("logical errors trans to 1d end.")
 
-                log("Origin syndrome trans to image syndrome start...")
+                # log("Origin syndrome trans to image syndrome start...")
                 image_syndromes = np.empty((batch_size, 2 * d - 1, 2 * d - 1))
                 for j in range(len(syndromes)):
                     image_syndromes[j] = ZX2image_sur(d, syndromes[j])
-                log("Origin syndrome trans to image syndrome end.")
+                # log("Origin syndrome trans to image syndrome end.")
 
-                log("Writing image syndromes to h5py file start...")
+                # log("Writing image syndromes to h5py file start...")
                 dataset_syndr.resize(dataset_syndr.shape[0] + batch_size, axis=0)
                 dataset_syndr[-batch_size:] = image_syndromes
-                log("Writing image syndromes to h5py file end.")
+                # log("Writing image syndromes to h5py file end.")
 
-                log("Writing logical errors to h5py file start...")
+                # log("Writing logical errors to h5py file start...")
                 dataset_le.resize(dataset_le.shape[0] + batch_size, axis=0)
                 dataset_le[-batch_size:] = logical_errors_1d.cpu().numpy()
-                log("Writing logical errors to h5py file end.")
+                # log("Writing logical errors to h5py file end.")
 
-                log("Num {} batch end.".format(i))
+                # log("Num {} batch end.".format(i))
         log("Error rate {} success!".format(p))
 
 
@@ -299,7 +299,7 @@ def gen_img_batch_eval():
     log("batch_size: {}".format(batch_size))
     for p in ps:
         log("Error rate now: {}".format(format(p, '.2f')))
-        file_name = "/root/Surface_code_and_Toric_code/{}_pe_img/{}_d{}_p{}_trnsz{}_imgsdr_eval_seed{}".format(c_type, c_type, d, format(p, '.3f'), trnsz, eval_seed)
+        file_name = "/root/Surface_code_and_Toric_code/{}_pe/{}_d{}_p{}_trnsz{}_imgsdr_eval_seed{}".format(c_type, c_type, d, format(p, '.3f'), trnsz, eval_seed)
         with h5py.File(file_name + ".hdf5", 'w') as f:
             log("Open h5py file success")
             dataset_syndr = f.create_dataset('image_syndromes', shape=(0, 2*d-1, 2*d-1), maxshape=(None, 2*d-1, 2*d-1), chunks=True, compression="gzip")
@@ -506,11 +506,26 @@ elif gtp == 'gbe':
 # nohup python gen_trndt_pe.py --c_type 'sur' --d 9 --k 1 --trnsz 10000000 --seed 0 --gtp gb --low_p 0.15 --high_p 0.20 --num_p 6 > logs/gen_trndt_pe_sur_d9_s0_3.log &
 
 # nohup python gen_trndt_pe.py --gtp gbt --c_type 'torc' --d 5 --k 2 --trnsz 10000000 --single_p 0.1 --seed 0 > logs/gen_trndt_pe_torc_d51345.log &
-# nohup python gen_trndt_pe.py --gtp gbt --c_type 'torc' --d 5 --k 2 --trnsz 10000000 --single_p 0.1 --eval_seed 1 > logs/gen_trndt_pe_torc_d534342.log &
+# nohup python gen_trndt_pe.py --gtp gbte --c_type 'torc' --d 5 --k 2 --trnsz 10000000 --single_p 0.1 --eval_seed 1 > logs/gen_trndt_pe_torc_d534342.log &
 
 # 7
 # nohup python gen_trndt_pe.py --gtp gbt --c_type 'torc' --d 7 --k 2 --trnsz 10000000 --single_p 0.1 --seed 0 > logs/gen_trndt_pe_torc_d51sfs345.log &
 # nohup python gen_trndt_pe.py --gtp gbt --c_type 'torc' --d 7 --k 2 --trnsz 10000000 --single_p 0.01 --seed 0 > logs/gen_trndt_pe_torc_d51sfs345.log &
+# nohup python gen_trndt_pe.py --gtp gbte --c_type 'torc' --d 7 --k 2 --trnsz 10000000 --single_p 0.01 --eval_seed 1 > logs/gen_trndt_pe_torc_d51sfs345.log &
 # nohup python gen_trndt_pe.py --gtp gbt --c_type 'torc' --d 7 --k 2 --trnsz 10000000 --single_p 0.05 --seed 0 > logs/gen_trndt_pe_torc_d51sfs345.log &
 # nohup python gen_trndt_pe.py --gtp gbt --c_type 'torc' --d 7 --k 2 --trnsz 10000000 --single_p 0.15 --seed 0 > logs/gen_trndt_pe_torc_d51sfs345.log &
 
+
+# 9
+# nohup python gen_trndt_pe.py --gtp gbt --c_type 'torc' --d 9 --k 2 --trnsz 10000000 --single_p 0.01 --seed 0 > logs/gen_trndt_pe_torc_d51sfsfs345.log &
+# nohup python gen_trndt_pe.py --gtp gbt --c_type 'torc' --d 9 --k 2 --trnsz 10000000 --single_p 0.05 --seed 0 > logs/gen_trndt_pe_torc_d51sf1sfs345.log &
+# nohup python gen_trndt_pe.py --gtp gbt --c_type 'torc' --d 9 --k 2 --trnsz 10000000 --single_p 0.10 --seed 0 > logs/gen_trndt_pe_torc_d51sf2sfs345.log &
+# nohup python gen_trndt_pe.py --gtp gbt --c_type 'torc' --d 9 --k 2 --trnsz 10000000 --single_p 0.15 --seed 0 > logs/gen_trndt_pe_torc_d51sf3sfs345.log &
+
+# sur 7
+# nohup python gen_trndt_pe.py --gtp gib --c_type 'sur' --d 7 --k 1 --trnsz 10000000 --single_p 0.01 --seed 0 > logs/gen_trndt_pe_torc_d51274345.log &
+# nohup python gen_trndt_pe.py --gtp gib --c_type 'sur' --d 7 --k 1 --trnsz 10000000 --single_p 0.05 --seed 0 > logs/gen_trndt_pe_torc_d5127s4345.log &
+# nohup python gen_trndt_pe.py --gtp gib --c_type 'sur' --d 7 --k 1 --trnsz 10000000 --single_p 0.10 --seed 0 > logs/gen_trndt_pe_torc_d5127ss4345.log &
+# nohup python gen_trndt_pe.py --gtp gib --c_type 'sur' --d 7 --k 1 --trnsz 10000000 --single_p 0.15 --seed 0 > logs/gen_trndt_pe_torc_d5127sss4345.log &
+
+# nohup python gen_trndt_pe.py --gtp gibe --c_type 'sur' --d 7 --k 1 --trnsz 10000 --single_p 0.01 > logs/gen_trndt_pe_torc_d512743e45.log &
