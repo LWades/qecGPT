fbebbe858465fc597b5b45fe80db4e6eb5a201de
lwades <601149711@qq.com>
2024-04-05 21:37:37 +0800

    dl

--- qec/decoding/set_num.py
+++ qec/decoding/set_num.py
@@ -7,5 +7,5 @@ sys.path.append(abspath(dirname(__file__)).strip('decoding'))
 from module import log
 
 with h5py.File(args.file, 'r') as f:
-    log(f['args.key'][()].shape[0])
+    log(f[args.key][()].shape[0])
 # python3 set_num.py --file '/root/Surface_code_and_Toric_code/sur_pe/sur_d13_p0.200_trnsz10000000_imgsdr_seed0.hdf5' --key image_syndromes
